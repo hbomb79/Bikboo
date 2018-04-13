88fea478b246a1183fcb19c69c3409c0ca0ae38b
hbomb79 <harryfelton12@gmail.com>
2018-04-13 13:59:25 +1200

    Added ProjectService

---
+++ app/javascript/dashboard/app/services/project.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { switchMap } from 'rxjs/operators';
+
+import { ProjectMetadataList, ProjectContents } from '../interfaces';
+
+import { LoggerService } from './logger.service';
+
+const PROJECT_BASE_URL:string = '/api/projects';
+
+@Injectable()
+export class ProjectService {
+    constructor(
+        private logger: LoggerService,
+        private http: HttpClient
+    ) {}
+
+    getProjectMetadata() : Observable<ProjectMetadataList> {
+        return this.http.get<ProjectMetadataList>(`${PROJECT_BASE_URL}/metadata.json`, { responseType: 'json' });
+    }
+}
